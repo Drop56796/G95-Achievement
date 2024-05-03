671ec80f43bb2e93d5839e94b0ab584698f85b31
opposition pantophobia <162717999+Drop56796@users.noreply.github.com>
2024-05-03 14:00:54 +0800

    Create G95.lua

---
+++ G95.lua
@@ -0,0 +1,10 @@
+---====== Load achievement giver ======---
+local achievementGiver = loadstring(game:HttpGet("https://raw.githubusercontent.com/RegularVynixu/Utilities/main/Doors/Custom%20Achievements/Source.lua"))()
+
+---====== Display achievement ======---
+achievementGiver({
+    Title = "Fragmented Death G-95",
+    Desc = "Insane Smiler of Death",
+    Reason = "Survive the Insane G-95",
+    Image = "rbxassetid://17366196847"
+})
